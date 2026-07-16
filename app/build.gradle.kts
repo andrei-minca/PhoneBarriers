111d8457bdc9684a09102f3d448cee79a15abbfc
andimin <andrei.minca@gmail.com>
2026-07-16 12:35:47 +0300

    upgraded android gradle plugin from version 8.13.2 to 9.3.0;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
     alias(libs.plugins.kotlin.android)
 
     // Add this line (version should match your Kotlin version: 2.0.21)
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10"
 
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" // Match Kotlin 2.0.21
+    id("com.google.devtools.ksp") version "2.3.2" // Match Kotlin 2.0.21
 
 }
 
